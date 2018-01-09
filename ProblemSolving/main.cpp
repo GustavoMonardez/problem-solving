87f8ddfa46c8df73e210285f6283c3e6c00204ad
RAINFOREST\Gustavo <gustavomonardez@gmail.com>
2018-01-09 13:49:20 -0800

    Set matrix zero Rotate matrix Next permutation

--- ProblemSolving/main.cpp
+++ ProblemSolving/main.cpp
@@ -1,8 +1,155 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
+template<typename T>
+void print2DVector(T& vec) {
+	for (auto row : vec) {
+		for (auto col : row)
+			cout << col << ", ";
+		cout << "\n";
+	}
+}
+template<typename T>
+void print1DVector(T& vec) {
+	for (auto val : vec)
+		cout << val << ",";
+	cout << "\n";
+}
+void setMatrixZeroes(vector<vector<int>>& matrix) {
+	if (matrix.empty())
+		return;
+	vector<vector<bool>>memo(matrix.size(), vector<bool>(matrix[0].size(), false));
+	for (auto row = 0; row < matrix.size(); ++row) {
+		for (auto col = 0; col < matrix[row].size(); ++col) {
+			if (matrix[row][col] == 0)
+				memo[row][col] = true;
+		}
+	}
+
+	for (auto row = 0; row < memo.size(); ++row) {
+		for (auto col = 0; col < memo[row].size(); ++col) {
+			if (memo[row][col] == true) {
+				//set row to zero
+				for (auto i = 0; i < matrix[row].size(); ++i) {
+					matrix[row][i] = 0;
+				}
+				//set col to zero
+				for (auto i = 0; i < matrix.size(); ++i) {
+					matrix[i][col] = 0;
+				}
+			}
+
+		}
+	}
+
+}
+void setMatrixZeroesDriver() {
+	vector<vector<int>>matrix{ {1,0,1},{1,1,1},{1,1,1} };
+	print2DVector(matrix);
+	cout << "\n";
+	setMatrixZeroes(matrix);
+	print2DVector(matrix);
+}
+void rotateMatrix(vector<vector<int>>& matrix) {
+	if (matrix.empty())
+		return;
+	int total_layers = matrix.size() / 2;
+	int top = 0;
+	int offset = 0;
+	int first = 0;
+	int last = 0;
+	//because a layer goes around the edge of the matrix
+	//top,right,bottom and left, it reduces our iterations to half
+	for (int layer = 0; layer < total_layers; ++layer) {
+		//moving inwards, so first element will change with
+		//each iteration
+		first = layer;
+		//to point to last element we need to subtract the current
+		//layer, because we're moving inwards and because zero base
+		//we need to subtract 1
+		last = matrix.size() - layer - 1;
+		for (int i = first; i < last; ++i) {
+			//moving inwards, so offset will be i minus first which
+			//equals layer
+			offset = i - first;
+			//capture top-left before overriding value
+			//row, in this case first, is the layer, which is why
+			//we don't increase it, just the col (i)
+			top = matrix[first][i];
+			//left->top (element in last row(bottom) and first col)
+			matrix[first][i] = matrix[last - offset][first];
+			//bottom->left (last row, last col)
+			matrix[last - offset][first] = matrix[last][last - offset];
+			//right->bottom (current row(i) and last col
+			matrix[last][last - offset] = matrix[i][last];
+			//top->right
+			matrix[i][last] = top;
+		}
+	}
+	
+}
+void rotateMatrixDriver() {
+	//vector<vector<int>>matrix{ {1,2},{3,4}};
+	vector<vector<int>>matrix{ {1,2,3},{4,5,6},{7,8,9} };
+	print2DVector(matrix);
+
+	cout << "\n";
+	rotateMatrix(matrix);
+	print2DVector(matrix);
+}
+void swapWith(int num, vector<int>& A, int i) {
+	int min = A[i];
+	int j = i, index = i;
+	for (j = i; j < A.size(); j++) {
+		if (min > A[j] && A[j] > A[num]) {
+			index = j;
+			min = A[j];
+		}
+	}
+	swap(A[index], A[num]);
+}
+void nextPermutation(vector<int>& nums) {
+	int end = nums.size() - 1;
+	bool found = false;
+	for (auto i = end; i > 0; --i) {
+		if (nums[i] > nums[i - 1]) {
+			//not sure what this function is doing
+			swapWith(i - 1, nums, i);
+			found = true;
+			break;
+		}
+	}
+	if (!found)
+		sort(nums.begin(), nums.end());
+}
+void nextPermutationDriver() {
+	vector<int>num1{ 1,2,3 };
+	vector<int>num2{ 3,2,1 };
+	vector<int>num3{ 1,1,5 };
+	vector<int>num4{ 20,50,113 };
+	print1DVector(num1);
+	print1DVector(num2);
+	print1DVector(num3);
+	print1DVector(num4);
+
+	nextPermutation(num1);
+	nextPermutation(num2);
+	nextPermutation(num3);
+	nextPermutation(num4);
+	print1DVector(num1);
+	print1DVector(num2);
+	print1DVector(num3);
+	print1DVector(num4);
+
+}
+
 
 int main(int argc, char** argv) {
-	cout << "Hello World!\n";
+	//setMatrixZeroesDriver();
+	//rotateMatrixDriver();
+	//nextPermutationDriver();
+
 	return 0;
 }
